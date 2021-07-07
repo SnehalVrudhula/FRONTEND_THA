699b6726afac64fd1744085d3145ddd60a2a2473
SnehalVrudhula <snehalvrudhula@gmail.com>
2021-07-07 20:20:35 +0530

    updated

---
+++ Day16/day16/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Meme from "./components/meme";
+import ChessBoard from "./components/chessboard"
+import React from "react";
+import "./main.css";
+
+
+function App (){
+  return(
+    
+    <div><Meme />
+  <ChessBoard /></div>
+  );
+}
+
+export default App;
---
+++ Day16/day16/src/components/meme.jsx
@@ -0,0 +1,12 @@
+import Meme_img from "./th.jpg";
+const Meme =()=>{
+    return(
+    <div className="container">
+        <img src={Meme_img} alt="Not supported" className="image"/>
+        <h1 className="memecard">MEME CARD</h1>
+    </div>
+    
+    );
+}
+
+export default Meme;
